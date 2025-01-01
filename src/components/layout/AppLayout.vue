979c687b27c903c1dcbee5ae532b2fea24edb05f
Jean Martha <tellojeanmartha@gmail.com>
2025-01-01 13:50:59 +0800

    first commit

---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+
+
+</script>
+
+<template>
+  <v-responsive>
+    <v-app :theme="theme">
+      <!-- App Bar -->
+      <v-app-bar class="px-3" elevation="3">
+        <span class="text-h6 font-weight-medium">CooKify</span>
+      </v-app-bar>
+
+      <!-- Main Content -->
+      <v-main>
+        <v-container>
+          <slot name="content"></slot>
+        </v-container>
+      </v-main>
+
+      <!-- Footer -->
+      <v-footer
+      
+        border
+        app
+        elevation="24"
+        class="mx-auto font-weight-bold"
+        :class="[theme === 'light' ? 'bg-surface-light' : 'bg-surface-dark']"
+      >
+        <span>© 2024 CooKify. All rights reserved.</span>
+      </v-footer>
+    </v-app>
+  </v-responsive>
+</template>
